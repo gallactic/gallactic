cfa978ea6abd14198ebc2442dd8981c522d0a3d4
Mostafa <mostafa.sedaghat@gmail.com>
2018-07-27 10:49:17 +0800

    Simplere key object

---
+++ keystore/key/encrypted.go
@@ -0,0 +1,13 @@
+package key
+
+// DecryptKeyFile returns an instance of Key object
+func DecryptKeyFile(file, auth string) (*Key, error) {
+
+	return nil, nil
+}
+
+// EncryptKeyFile encrypts a key and return encrypted byte array
+func EncryptKeyFile(key *Key, auth, file string) error {
+
+	return nil
+}
