cd2f0ebc07710e1e49a65a0100eb309541bc3101
ToniyaSundaram <toniya.sundaram@gmail.com>
2018-07-27 14:25:41 +0800

    Adding Encrypt and Decrypt functionality to KeyStore

--- keystore/key/key_test.go
+++ keystore/key/key_test.go
@@ -1,19 +1,38 @@
 package key
 
 import (
+	"encoding/json"
 	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestEncryption(t *testing.T) {
+	auth := "1234"
+	//Generates Private Key
 	k1 := GenAccountKey()
 
 	fname := fmt.Sprintf("%s.json", k1.Address().String())
-	err := EncryptKeyFile(k1, fname, "1234")
+
+	//Encrypts Private Key
+	data, err := EncryptKeyFile(k1, auth)
 	assert.NoError(t, err)
-	k2, err := DecryptKeyFile(fname, "1234")
+
+	err = storeNewKey(k1, auth, fname)
 	assert.NoError(t, err)
-	assert.Equal(t, k1, k2)
+
+	kj := new(encryptedKeyJSONV3)
+	if err := json.Unmarshal(data, kj); err != nil {
+		assert.NoError(t, err)
+	}
+
+	//Decrypts Json Object
+	k2, err := DecryptKeyFile(filePath+fname, auth)
+	assert.NoError(t, err)
+
+	if !reflect.DeepEqual(k1.data.PrivateKey, k2.data.PrivateKey) {
+		t.Fatal(err)
+	}
 }
