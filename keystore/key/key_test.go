35ae9eea0dcac295fd2da450d3249cbc6c077677
ToniyaSundaram <toniya.sundaram@gmail.com>
2018-07-30 11:04:26 +0800

    Addressed the code comments for keyStore

--- keystore/key/key_test.go
+++ keystore/key/key_test.go
@@ -2,7 +2,6 @@ package key
 
 import (
 	"fmt"
-	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,24 +11,12 @@ func TestEncryption(t *testing.T) {
 	auth := "1234"
 	//Generates Private Key
 	k1 := GenAccountKey()
-
 	fname := fmt.Sprintf("%s.json", k1.Address().String())
-
 	//Encrypts the key json blob
 	err := EncryptKeyFile(k1, auth, fname)
 	assert.NoError(t, err)
-
 	//Decrypts Json Object
 	k2, err := DecryptKeyFile(auth, fname)
 	assert.NoError(t, err)
-
-	if !reflect.DeepEqual(k1.data.Address, k2.data.Address) {
-		t.Fatal(err)
-	}
-	if !reflect.DeepEqual(k1.data.PublicKey, k2.data.PublicKey) {
-		t.Fatal(err)
-	}
-	if !reflect.DeepEqual(k1.data.PrivateKey, k2.data.PrivateKey) {
-		t.Fatal(err)
-	}
+	assert.Equal(t, k1, k2)
 }
