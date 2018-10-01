6175d0281ca18d4ae6dfba734bb2d2ec0963e80f
Gheis Mohammadi <Gheis.Mohammadi@gmail.com>
2018-10-01 16:34:23 +0800

    added contract code

--- core/evm/sputnik/sputnik_test.go
+++ core/evm/sputnik/sputnik_test.go
@@ -72,7 +72,7 @@ func TestSputnikVM(t *testing.T) {
 	//Deploy Contract...
 	outD, errDeploy := Execute(bc, cache, caller, callee, txDep, &gas, true)
 	require.NoError(t, errDeploy)
-	require.Equal(t,hex.EncodeToString(outD),getContractCodeAfterDeploy())
+	require.Equal(t, hex.EncodeToString(outD), getContractCodeAfterDeploy())
 
 	//Call Set Method by 1234567...
 	setMethod, _ := hex.DecodeString("60fe47b100000000000000000000000000000000000000000000000000000000000001c8")
@@ -86,10 +86,30 @@ func TestSputnikVM(t *testing.T) {
 	txGet, _ := tx.NewCallTx(caller.Address(), callee.Address(), 1, getMethod, 10000000, 0, 1)
 	outG, errGet := Execute(bc, cache, caller, callee, txGet, &gas, false)
 	require.NoError(t, errGet)
-	require.Equal(t,hex.EncodeToString(outG),"00000000000000000000000000000000000000000000000000000000000001c8")
+	require.Equal(t, hex.EncodeToString(outG), "00000000000000000000000000000000000000000000000000000000000001c8")
 }
 
 func createContractCode() []byte {
+	//Test Smart Contract
+	/*
+		pragma solidity ^0.4.24;
+		contract SimpleStorage {
+			uint private _balance;
+			uint private _storedData;
+			event notifyStorage(uint x);
+			constructor() public payable {
+				_storedData = 0x12d687;
+				_balance = 1500000;
+			}
+			function set(uint x) public payable {
+				_storedData = x;
+				emit notifyStorage(x);
+			}
+			function get() public view returns (uint) {
+				return _storedData;
+			}
+		}
+	*/
 	deployCode, _ := hex.DecodeString("60806040526212d6876001556216e36060005560e9806100206000396000f30060806040526004361060485763ffffffff7c010000000000000000000000000000000000000000000000000000000060003504166360fe47b18114604d5780636d4ce63c146058575b600080fd5b6056600435607c565b005b348015606357600080fd5b50606a60b7565b60408051918252519081900360200190f35b60018190556040805182815290517f23f9887eb044d32dba99d7b0b753c61c3c3b72d70ff0addb9a843542fd7642129181900360200190a150565b600154905600a165627a7a7230582013452558cc58a514b8056c0b45a3f1ab8c5f736b2e087c65e615650b562415ff0029")
 	return deployCode
 }
