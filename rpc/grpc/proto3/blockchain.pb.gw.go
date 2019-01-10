969973a352d23e019a0a2d8e0f0877dae42d0f6d
Nagaraj <nagaraj.manjunath@finterra.org>
2019-01-10 17:15:39 +0800

    updated in blockchaininfo method

--- rpc/grpc/proto3/blockchain.pb.gw.go
+++ rpc/grpc/proto3/blockchain.pb.gw.go
@@ -275,6 +275,15 @@ func request_BlockChain_GetBlocks_0(ctx context.Context, marshaler runtime.Marsh
 
 }
 
+func request_BlockChain_GetBlockchainInfo_0(ctx context.Context, marshaler runtime.Marshaler, client BlockChainClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq Empty
+	var metadata runtime.ServerMetadata
+
+	msg, err := client.GetBlockchainInfo(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 var (
 	filter_BlockChain_GetBlockTxs_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
 )
@@ -315,33 +324,6 @@ func request_BlockChain_GetBlockTxs_1(ctx context.Context, marshaler runtime.Mar
 	}
 
 	msg, err := client.GetBlockTxs(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
-func request_BlockChain_GetBlockchainInfo_0(ctx context.Context, marshaler runtime.Marshaler, client BlockChainClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq BlockchainInfoRequest
-	var metadata runtime.ServerMetadata
-
-	var (
-		val string
-		ok  bool
-		err error
-		_   = err
-	)
-
-	val, ok = pathParams["Blockwithin"]
-	if !ok {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "Blockwithin")
-	}
-
-	protoReq.Blockwithin, err = runtime.String(val)
-
-	if err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "Blockwithin", err)
-	}
-
-	msg, err := client.GetBlockchainInfo(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
 }
@@ -644,6 +626,26 @@ func RegisterBlockChainHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 
 	})
 
+	mux.Handle("GET", pattern_BlockChain_GetBlockchainInfo_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_BlockChain_GetBlockchainInfo_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_BlockChain_GetBlockchainInfo_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("GET", pattern_BlockChain_GetBlockTxs_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -714,6 +716,8 @@ var (
 
 	pattern_BlockChain_GetBlocks_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 1, 0, 4, 1, 5, 2}, []string{"Blocks", "minHeight", "maxHeight"}, ""))
 
+	pattern_BlockChain_GetBlockchainInfo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"GetBlockchainInfo"}, ""))
+
 	pattern_BlockChain_GetBlockTxs_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"BlockTxs"}, ""))
 
 	pattern_BlockChain_GetBlockTxs_1 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1}, []string{"BlockTxs", "height"}, ""))
@@ -745,6 +749,8 @@ var (
 	forward_BlockChain_GetBlock_0 = runtime.ForwardResponseMessage
 
 	forward_BlockChain_GetBlocks_0 = runtime.ForwardResponseMessage
+
+	forward_BlockChain_GetBlockchainInfo_0 = runtime.ForwardResponseMessage
 
 	forward_BlockChain_GetBlockTxs_0 = runtime.ForwardResponseMessage
 
